36e1d725b16c17265ae3953464fd6de8e0d0e529
Safaeat <89898173+Safaeat@users.noreply.github.com>
2023-10-23 23:35:47 +0600

    Add files via upload

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+$m=5; $x=5-1; $a=20-$m++;
+$s= "welcome to Southeast University ";
+echo " The value of M and A is: " . $k . " " . ++$a;
+$n= $a%2;
+$n=$k+$n;
+echo " </br>"
+echo " The value of N =" . $k;
+if ($m> 45) { echo " N is greater than 45 </br>"; }
+var_dump($x==6);
+if(3!= $x)
+{
+echo " Value of x not three ";
+}
+else
+echo " Value of x is three "
+echo $s;
+?>
